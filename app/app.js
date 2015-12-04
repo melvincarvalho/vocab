d8b8dad0e89057af56beb9c21422c20c038db4ab
Melvin Carvalho <melvincarvalho@gmail.com>
2015-12-04 23:31:25 +0100

    add alerts

--- app/app.js
+++ app/app.js
@@ -519,6 +519,29 @@ App.controller('Main', function($scope, $http, $location, $timeout, ngAudio, LxN
     });
   };
 
+  $scope.alert = function() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification($scope.first, undefined, $scope.second);
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification($scope.first, undefined, $scope.second);
+        }
+      });
+    }
+  };
+
 
   $scope.initApp();
 
